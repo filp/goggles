9de051a86eb48ba76ec2763885ba36975f293c7f
filp <dobreira@gmail.com>
2013-02-13 16:45:35 +0000

    Add ResultCollection skeleton

--- src/Goggles/Result/ResultCollection.php
+++ src/Goggles/Result/ResultCollection.php
@@ -6,12 +6,52 @@
  */
 
 namespace Goggles\Result;
+use Goggles\Result\Result;
 
 /**
  * Goggles\Result\ResultCollection
  */
 class ResultCollection
 {
+    /**
+     * @var Goggles\Result\Result[]
+     */
+    protected $results;
 
+    /**
+     * Returns an array with all available results,
+     * or an empty array if no results are available.
+     * @return Goggles\Result\Result[]
+     */
+    public function getResultsArray()
+    {
+        return $this->results !== null ? $this->results : array();
+    }
+
+    /**
+     * @param  Goggles\Result\Result
+     * @return Goggles\Result\ResultCollection
+     */
+    public function addResult(Result $result)
+    {
+        if($this->results === null) {
+            $this->results = array();
+        }
+
+        $this->results[] = $result;
+    }
+
+    /**
+     * @param  Goggles\Result\Result[]
+     * @return Goggles\Result\ResultCollection
+     */
+    public function addResults(array $results)
+    {
+        foreach($results as $result) {
+            $this->addResult($result);
+        }
+
+        return $this;
+    }
 }
 
